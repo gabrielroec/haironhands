0936eb4462ad71813a5268b0cb3a790cfc047f8c
RoecDesenvolvimentoWeb <roec.roecdesenvolvimentoweb@gmail.com>
2024-07-01 11:07:55 -0300

    update db function

--- ws/src/routes/servico.routes.js
+++ ws/src/routes/servico.routes.js
@@ -6,9 +6,7 @@ const Servico = require("../models/servico");
 const Arquivos = require("../models/arquivos");
 // const moment = require("moment");
 
-/*
-  FAZER NA #01
-*/
+//ROTA RECEBE FORM DATA
 router.post("/", async (req, res) => {
   var busboy = new Busboy({ headers: req.headers });
   busboy.on("finish", async () => {
@@ -59,6 +57,62 @@ router.post("/", async (req, res) => {
       await Arquivos.insertMany(arquivos);
 
       res.json({ error: false, arquivos, servico });
+    } catch (err) {
+      res.json({ error: true, message: err.message });
+    }
+  });
+  req.pipe(busboy);
+});
+
+router.put("/:id", async (req, res) => {
+  var busboy = new Busboy({ headers: req.headers });
+  busboy.on("finish", async () => {
+    try {
+      let errors = [];
+      let arquivos = [];
+
+      if (req.files && Object.keys(req.files).length > 0) {
+        for (let key of Object.keys(req.files)) {
+          const file = req.files[key];
+
+          const nameParts = file.name.split(".");
+          const fileName = `${new Date().getTime()}.${
+            nameParts[nameParts.length - 1]
+          }`;
+          const path = `servicos/${req.body.salaoId}/${fileName}`;
+
+          const response = await aws.uploadToS3(
+            file,
+            path
+            //, acl = https://docs.aws.amazon.com/pt_br/AmazonS3/latest/dev/acl-overview.html
+          );
+
+          if (response.error) {
+            errors.push({ error: true, message: response.message.message });
+          } else {
+            arquivos.push(path);
+          }
+        }
+      }
+
+      if (errors.length > 0) {
+        res.json(errors[0]);
+        return false;
+      }
+
+      //  ATUALIZAR SERVIÇO
+      let jsonServico = JSON.parse(req.body.servico);
+      await Servico.findByIdAndUpdate(req.params.id, jsonServico);
+
+      // CRIAR ARQUIVO
+      arquivos = arquivos.map((arquivo) => ({
+        referenciaId: req.params.id,
+        model: "Servico",
+        arquivo,
+      }));
+      await Arquivos.insertMany(arquivos);
+
+      res.json({ error: false, jsonServico });
     } catch (err) {
       res.json({ error: true, message: err.message });
     }
